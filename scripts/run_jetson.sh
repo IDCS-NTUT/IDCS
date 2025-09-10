2f12d8432c0edb4ecdab0758c5a0e7fc1f220c05
IDCS <0911allahuakbar@gmail.com>
2025-09-10 23:51:04 +0800

    modified:   scripts/run_jetson.sh

--- scripts/run_jetson.sh
+++ scripts/run_jetson.sh
@@ -3,6 +3,4 @@ export LD_PRELOAD="/usr/lib/aarch64-linux-gnu/libGLdispatch.so.0:/usr/lib/aarch6
 unset DISPLAY
 export QT_QPA_PLATFORM=offscreen
 set -euo pipefail
-sudo nvpmodel -m 2 || true
-sudo jetson_clocks || true
 python -m jetson.server --config configs/dev.yaml

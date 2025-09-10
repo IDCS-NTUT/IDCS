a868fc1ad7e87458dc52efc3bd2cbf9b0a175185
IDCS <0911allahuakbar@gmail.com>
2025-09-10 11:38:11 +0800

    modified:   scripts/run_jetson.sh

--- scripts/run_jetson.sh
+++ scripts/run_jetson.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export LD_PRELOAD="/usr/lib/aarch64-linux-gnu/libGLdispatch.so.0:/usr/lib/aarch64-linux-gnu/libGL.so.1"
 unset DISPLAY
 export QT_QPA_PLATFORM=offscreen
 set -euo pipefail

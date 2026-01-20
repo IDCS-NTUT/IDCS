15d90606260979188592ed1ef01461ada45feae1
IDCS-NTUT <0911allahuakbar@gmail.com>
2026-01-20 23:02:31 +0800

    Define startup stop commands before use

--- jetson/gimbal_bridge.py
+++ jetson/gimbal_bridge.py
@@ -441,6 +441,65 @@ def main() -> int:
             )
         )
 
+    def _send_stop_commands(reason: str) -> None:
+        _LOG.info("sending gimbal stop commands (%s)", reason)
+        stop_cmds = [
+            _build_command(
+                cmd_id="stop:yaw",
+                func="F6",
+                addr=yaw_addr,
+                payload=_encode_speed_cmd(
+                    0.0, acc=yaw_accel, gear_ratio=yaw_ratio, max_rate=yaw_rate_limit
+                ),
+                expect_reply=False,
+                expected_len=None,
+                priority="critical",
+                target=serial_target,
+            ),
+            _build_command(
+                cmd_id="stop:pitch",
+                func="F6",
+                addr=pitch_group_addr,
+                payload=_encode_speed_cmd(
+                    0.0,
+                    acc=pitch_accel,
+                    gear_ratio=pitch_ratio,
+                    max_rate=pitch_rate_limit,
+                ),
+                expect_reply=False,
+                expected_len=None,
+                priority="critical",
+                target=serial_target,
+            ),
+            _build_command(
+                cmd_id="disable:yaw",
+                func="F3",
+                addr=yaw_addr,
+                payload=[0x00],
+                expect_reply=False,
+                expected_len=None,
+                priority="critical",
+                target=serial_target,
+            ),
+            _build_command(
+                cmd_id="disable:pitch",
+                func="F3",
+                addr=pitch_group_addr,
+                payload=[0x00],
+                expect_reply=False,
+                expected_len=None,
+                priority="critical",
+                target=serial_target,
+            ),
+        ]
+        update_pub.send_update(
+            _build_update(
+                source="jetson.gimbal_bridge",
+                target=serial_target,
+                commands=stop_cmds,
+            )
+        )
+
     startup_attempt = 0
     while True:
         startup_attempt += 1
@@ -570,64 +629,6 @@ def main() -> int:
 
     yaw_counts: Optional[int] = None
     pitch_counts: dict[int, int] = {}
-    def _send_stop_commands(reason: str) -> None:
-        _LOG.info("sending gimbal stop commands (%s)", reason)
-        stop_cmds = [
-            _build_command(
-                cmd_id="stop:yaw",
-                func="F6",
-                addr=yaw_addr,
-                payload=_encode_speed_cmd(
-                    0.0, acc=yaw_accel, gear_ratio=yaw_ratio, max_rate=yaw_rate_limit
-                ),
-                expect_reply=False,
-                expected_len=None,
-                priority="critical",
-                target=serial_target,
-            ),
-            _build_command(
-                cmd_id="stop:pitch",
-                func="F6",
-                addr=pitch_group_addr,
-                payload=_encode_speed_cmd(
-                    0.0,
-                    acc=pitch_accel,
-                    gear_ratio=pitch_ratio,
-                    max_rate=pitch_rate_limit,
-                ),
-                expect_reply=False,
-                expected_len=None,
-                priority="critical",
-                target=serial_target,
-            ),
-            _build_command(
-                cmd_id="disable:yaw",
-                func="F3",
-                addr=yaw_addr,
-                payload=[0x00],
-                expect_reply=False,
-                expected_len=None,
-                priority="critical",
-                target=serial_target,
-            ),
-            _build_command(
-                cmd_id="disable:pitch",
-                func="F3",
-                addr=pitch_group_addr,
-                payload=[0x00],
-                expect_reply=False,
-                expected_len=None,
-                priority="critical",
-                target=serial_target,
-            ),
-        ]
-        update_pub.send_update(
-            _build_update(
-                source="jetson.gimbal_bridge",
-                target=serial_target,
-                commands=stop_cmds,
-            )
-        )
 
     stop_commands_sent = False
     try:

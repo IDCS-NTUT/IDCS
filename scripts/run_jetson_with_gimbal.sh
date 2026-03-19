c44469684bdb902df15fc3530f2c4a8facb5a498
idcs <0911allahuakbar@gmail.com>
2026-03-19 23:37:21 +0800

    Add support for gimbal mode in Jetson server and update script to enable it

--- scripts/run_jetson_with_gimbal.sh
+++ scripts/run_jetson_with_gimbal.sh
@@ -26,6 +26,7 @@ trap cleanup EXIT
 export LD_PRELOAD="/usr/lib/aarch64-linux-gnu/libGLdispatch.so.0:/usr/lib/aarch64-linux-gnu/libGL.so.1"
 unset DISPLAY
 export QT_QPA_PLATFORM=offscreen
+export JETSON_WITH_GIMBAL=1
 
 SERIAL_PORT=$(python - "$CONFIG_PATH" "$EXTRA_PATH" <<'PY'
 import sys
